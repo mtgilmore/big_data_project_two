cdaeb7039c812232aee926272c673733f98273f0
Matt Gilmore <mtgilmore@comcast.net>
2020-03-23 15:01:47 -0400

    Add data and environment

--- script.R
+++ script.R
@@ -1,2 +1,7 @@
 print("hello")
 
+path = "C:/Users/mtgil/OneDrive/Documents/GitHub/big_data_project_two"
+
+names = c("age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "class")
+
+data = read.csv(paste(path, "/adult.data", sep=""), header=FALSE, col.names = names)
