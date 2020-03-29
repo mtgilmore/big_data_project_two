e59b70059cb7e91d11a6e97e3db07775bc7cd530
Matt Gilmore <mtgilmore@comcast.net>
2020-03-28 20:02:14 -0400

    Add some summarization

--- script.R
+++ script.R
@@ -1,7 +1,13 @@
-print("hello")
+install.packages("psych")
+library(psych)
 
+# My file path to data
 path = "C:/Users/mtgil/OneDrive/Documents/GitHub/big_data_project_two"
 
+# Importing data
 names = c("age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "class")
-
 data = read.csv(paste(path, "/adult.data", sep=""), header=FALSE, col.names = names)
+
+# Stats for our report
+summary(data)
+describe(data)
